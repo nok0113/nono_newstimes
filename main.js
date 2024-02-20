22d3aa6c1ee61078752b167099c222cd47e993cb
A-Ri Lee <dldkfl32@naver.com>
2024-02-21 01:43:08 +0900

    hihi

---
+++ main.js
@@ -0,0 +1,12 @@
+const API_KEY=`9d48dfe9ca6d40ae8e444820b2ce9e7f`;
+let news = [];
+const getLatestNews = async() => {
+    const url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`);
+const response = await fetch(url);
+const data = await response.json()
+
+news = data.articles;
+console.log("ddd", data.articles);
+};
+
+getLatestNews();
