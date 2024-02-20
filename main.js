5bd4c55b50cb2b57d4e29eb60be59877426d5d14
A-Ri Lee <dldkfl32@naver.com>
2024-02-21 02:19:44 +0900

    525224k

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
-const API_KEY=`9d48dfe9ca6d40ae8e444820b2ce9e7f`;
+let API_KEY=`9d48dfe9ca6d40ae8e444820b2ce9e7f`;
 let news = [];
 const getLatestNews = async() => {
-    const url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`);
+    const url = new URL(`https://genuine-llama-77c90b.netlify.app/top-headlines?country=kr&apiKey=${API_KEY}`);
+    //new URL(`https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`);
 const response = await fetch(url);
 const data = await response.json()
 
 news = data.articles;
-console.log("ddd", data.articles);
+console.log("ddd", news);
 };
 
 getLatestNews();
+
